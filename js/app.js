761b52d653c552a3877b80016e886bccb011eaac
Jelson J <jelson20outlook.com>
2020-06-27 18:18:19 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 'use strict';
+let backgrColor, fontColor, nameColor;
+
+function getColor() {
+	nameColor = document.querySelector('#favourColor').value;
+	localStorage.backColor = backgrColor;
+	localStorage.textColor = fontColor;
+	localStorage.name = nameColor;
+}
 
 function setColor(index, color) {
 	if (index === 1) {
 		document.body.style.backgroundColor = color;
+		backgrColor = color;
 	} else {
 		document.body.style.color = color;
+		fontColor = color;
 	}
 }
